57bc40b439ceac0042df31860a0beb6eb2411536
ToufikGouri <toufikgouri3@gmail.com>
2023-12-29 13:12:28 +0530

    Adding "fetch notes" functionality to iNotebook

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,14 +1,18 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import noteContext from '../context/notes/noteContext'
 import NoteItem from './NoteItem'
 import AddNote from './AddNote'
 
 const Notes = () => {
-    const context = useContext(noteContext)
-    const { notes } = context
+    const context = useContext(noteContext);
+    const { notes, getNotes } = context;
+    useEffect(() => {
+        getNotes()
+    }, [])
+
     return (
         <>
-        <AddNote/>
+            <AddNote />
             <div className="row my-3">
                 <h2>Your notes</h2>
                 {notes.map((note) => {
