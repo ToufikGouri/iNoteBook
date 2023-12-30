2528441125a1203ef4a1f929e3ca3dc23fedd351
ToufikGouri <toufikgouri3@gmail.com>
2023-12-30 14:35:05 +0530

    Adding Login Component to iNotebook

--- BackEnd/routes/auth.js
+++ BackEnd/routes/auth.js
@@ -49,12 +49,13 @@ router.post('/createuser', [
         res.status(500).send("Internal server Error");
     }
 })
- 
+
 // ROUTE 2:  Authenticate a User using: POST "/api/auth/login". No login required
 router.post('/login', [
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password cannot be blank').exists(),
 ], async (req, res) => {
+    let success = false
     // If there are errors, return bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -65,12 +66,14 @@ router.post('/login', [
     try {
         let user = await User.findOne({ email })
         if (!user) {
+            success = false
             return res.status(400).json({ error: "Please try to login with correct credentials" })
         }
 
         const passwordCompare = await bcrypt.compare(password, user.password)
         if (!passwordCompare) {
-            return res.status(400).json({ error: "Please try to login with correct credentials" })
+            success = false
+            return res.status(400).json({ success, error: "Please try to login with correct credentials" })
         }
 
         const data = {
@@ -79,7 +82,8 @@ router.post('/login', [
             }
         }
         const authtoken = jwt.sign(data, JWT_SECRET)
-        res.json({ authtoken })
+        success = true
+        res.json({ success, authtoken })
 
     } catch (error) {
         console.error(error.message);
---
+++ src/components/Signup.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Signup = () => {
+    return (
+        <>
+            I'm signup
+        </>
+    )
+}
+
+export default Signup
