842f566e3a2f4fdf05e496179428fa2dd55e5192
ToufikGouri <toufikgouri3@gmail.com>
2024-01-05 10:24:51 +0530

    Setting up React-Spring & logout page-end edited

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { animated, useSpring } from 'react-spring'
 
 const Logout = () => {
 
     // Animation logic
-    let allTexts = ["plan", "mood", "feelings", "goals", "thoughts", "vision", "targets"]
+    let allTexts = ["Plan", "Mood", "Feelings", "Goals", "Thoughts", "Vision", "Ideas", "Targets"]
+    const [currentText, setCurrentText] = useState(allTexts[0])
     let text = document.getElementById("lo-animatingText");
-    setInterval(() => {
-        text.innerHTML = Math.random()
-        // multily by length of the array and take a word
-    }, 1000);
-    // text.style.color = "yellow"
+
+    useEffect(() => {
+        const setInt = setInterval(() => {
+            const currentIndex = allTexts.indexOf(currentText)
+            const nextIndex = (currentIndex + 1) % allTexts.length
+            setCurrentText(allTexts[nextIndex])
+        }, 4000);
+        // cleanup line executes after the component is unmounted, till then the code runs
+        return () => clearInterval(setInt)
+    }, [allTexts, currentText])
+
+    // React-useSpring effect WORD Effect
+    const animSpring = useSpring({
+        from: { opacity: 0, transform: 'rotateX(45deg) translateY(100%)' },
+        to: { opacity: 1, transform: 'rotateX(0deg) translateY(0%)' },
+        reset: true,
+    });
+
+    // React-useSpring effect COUNTER Effect
+    const { count } = useSpring({
+        from: { count: 0, opacity: 0 },
+        to: { count: 150000, opacity: 1 }
+    })
+    // interpolate property is used here and it's working but find a way to do without using interpolate
+    // becuase it's depricated now
 
     return (
         <>
@@ -18,7 +40,7 @@ const Logout = () => {
                 {/* ***************************Hero Sectionc****************************** */}
 
                 <div className="lo-heroSection my-3">
-                    <h1 className='lo-headings'>The simplest way to keep notes</h1>
+                    <h1 className='lo-headings'>The Simplest Way To Keep Notes</h1>
                     <p>Simplifying Your Thoughts - Organize, Create, and Access Your Ideas Effortlessly, All Your Notes At One Place.</p>
                     <button className="btn btn-primary">Create Your Free Account</button>
                 </div>
@@ -26,12 +48,14 @@ const Logout = () => {
                 {/* ***************************Hero Animation****************************** */}
 
                 <div className='lo-animationContainer m-5'>
-                    <h1 className="lo-headings">What's your <span id='lo-animatingText'>idea</span> today ?</h1>
+                    <h1 className="lo-headings">What Is Your Today's
+                        <animated.div style={{ ...animSpring }} id='lo-animatingText'>{currentText + " ?"}</animated.div>
+                    </h1>
                 </div>
 
                 {/* ***************************Hero Sectionc mid****************************** */}
 
-                <div className="lo-mid">
+                <div className="lo-mid border-bottom pb-3">
 
                     <div className="row lo-imgParent">
                         <div className="col-md-6">
@@ -66,8 +90,24 @@ const Logout = () => {
                         </div>
                     </div>
 
-
                 </div>
+
+                {/* ***************************Hero Sectionc End****************************** */}
+
+                <div className="lo-end my-5">
+                    <h1 className="lo-headings text-center mb-4">What people are saying</h1>
+                    <div className="row">
+                        <p className="col-md-4">If you're not using iNoteBook, you're missing out.<div>TechCrunch</div></p>
+                        <p className="col-md-4">If you're looking for a cross-platform note-taking tool with just enough frills, it's hard to look beyond iNoteBook.<div>MacWorld</div></p>
+                        <p className="col-md-4">If you want a truly distraction-free environment then you can't do better than iNoteBook for your note-taking needs.<div>Zapier</div></p>
+                    </div>
+
+                    {/* Counter animation */}
+                    <div className="counter my-5">
+                        <h1 className="lo-headings text-center">Join The Family Of <animated.div className="d-inline-block">{count.interpolate((val) => Math.floor(val))}</animated.div> Users On iNoteBook</h1>
+                    </div>
+                </div>
+
             </div>
         </>
     )
